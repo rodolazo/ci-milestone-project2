a06bde87b5971ce8edeb94c63d4fc9cd049efb58
rodolazo <rodolfo.lazo@yahoo.com>
2021-12-18 06:50:22 +0000

    Set and declare the methods we are going to use in the game

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+let game = {
+    /**
+     * Method: Initialize the game
+     */
+    begin: function() {
+    },
+    
+    /**
+     * Verify the option selected by the user
+     * @param {integer} userSelected 
+     */
+    checkAnswer: function(userSelected) {
+    },
+    
+    /**
+     * Update position of the Array
+     */
+    increasePos: function() {
+    },
+    
+    /**
+     * Print score on the board game
+     */
+    updateScore: function() {
+    },
+    
+    //Print result of selection on the board game
+    showResult: function(isCorrect) {        
+    }
+    
+};
+  
+// begin the application
+game.begin();
+  
