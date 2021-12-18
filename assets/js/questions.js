6aec3a6d12c6745bd26ecd6dc0469baa5721ee30
rodolazo <rodolfo.lazo@yahoo.com>
2021-12-18 06:42:11 +0000

    Build array of objects

---
+++ assets/js/questions.js
@@ -0,0 +1,18 @@
+// Array with object questions
+let questions = [
+    {
+      spanish: 'Levantarse',
+      alternatives: ['Talk', 'Stand up', 'Erase', 'Sleep'],
+      correctAnswer: 1
+    },
+    {
+      spanish: 'Nombre',
+      alternatives: ['Name', 'teacher', 'last name', 'signature'],
+      correctAnswer: 0
+    },
+    {
+      spanish: 'Bolsillo',
+      alternatives: ['Waistband', 'Collar', 'Pocket', 'Cuff'],
+      correctAnswer: 2
+    },    
+  ];
